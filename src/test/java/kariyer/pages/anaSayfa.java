e4fbe33acef5f7ab276060ff9434c8df9a2b7165
ms-joker <joker728@gmail.com>
2024-10-09 16:35:43 +0300

    Added several files.

--- src/test/java/kariyer/pages/anaSayfa.java
+++ src/test/java/kariyer/pages/anaSayfa.java
@@ -1,5 +1,12 @@
 package kariyer.pages;
 
+import org.openqa.selenium.support.PageFactory;
+
+import java.sql.Driver;
+
 public class anaSayfa {
+    public anaSayfa() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
 }
