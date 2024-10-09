22dc502aaf25e313f0502d512a7a3d18e845ee88
ms-joker <joker728@gmail.com>
2024-10-09 20:55:38 +0300

    Did some update here.

---
+++ src/test/java/kariyer/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package kariyer.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin={
+                "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-repeat.xml",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.repor.PrettyReports:target/cucumber"
+        },
+        feature = "src/test/resources/features",
+        glue = "java/kariyer/step_definition",
+        dryRun=false,
+        tag = "",
+        publish = true
+)
+public class CukesRunner {
+}
